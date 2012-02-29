d7a2ccf71578deec87824b75e1e24e9944f8d92b
Rick Vause <rvause@gmail.com>
2012-02-29 18:26:10 +0100

    Small indentation error

--- src/app/settings.py
+++ src/app/settings.py
@@ -83,9 +83,9 @@ ROOT_URLCONF = 'app.project.urls'
 
 # List of callables that know how to import templates from various sources.
 TEMPLATE_LOADERS = (
-   'django.template.loaders.filesystem.Loader',
-   'django.template.loaders.app_directories.Loader',
-   'django.template.loaders.eggs.Loader',
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+    'django.template.loaders.eggs.Loader',
 )
 
 TEMPLATE_CONTEXT_PROCESSORS =  (
